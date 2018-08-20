7727416096fa8d10f2e9c2d9e9b158aab9936f90
Effat Chaar <effat.shaar@gmail.com>
2018-08-19 19:55:09 -0700

    working on refill inventory function but having one failing test

--- __tests__/vending-machine.spec.js
+++ __tests__/vending-machine.spec.js
@@ -13,4 +13,23 @@ describe('Vending Machine', () => {
             })
         })
     })
+    //refill
+    describe('When refilling the inventory', () => {
+        it('Should throw an error if the item is not found', () => {
+            expect(vendingMachine.inventoryRefill('Milk', 3)).toEqual('Not valid!')
+        })
+        it('Should return the object of the item that has been refilled', () => {
+            expect(vendingMachine.inventoryRefill('Galaxy', 10)).toEqual(
+                {'count': 10, 'name': 'Galaxy'
+            })
+        })
+        it('Should throw an error if the item is not a string', () => {
+            expect(vendingMachine.inventoryRefill(10, 3)).toEqual('Not valid!')
+        })
+        it('Should throw an error if amount is not an integer', () => {
+            expect(vendingMachine.inventoryRefill('Galaxy', 'two')).toEqual(
+                'Please insert appropiate numbers to indicate amount'
+            )
+        })
+    })
 })
