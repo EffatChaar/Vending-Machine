9669db7a4c340314e6040d28a4f1af3c4429e00b
Effat Chaar <effat.shaar@gmail.com>
2018-08-19 20:31:51 -0700

    passed all the tests for change resupply function

--- __tests__/vending-machine.spec.js
+++ __tests__/vending-machine.spec.js
@@ -16,20 +16,37 @@ describe('Vending Machine', () => {
     //refill
     describe('When refilling the inventory', () => {
         it('Should throw an error if the item is not found', () => {
-            expect(vendingMachine.inventoryRefill('Milk', 3)).toEqual('Not valid!')
+            expect(vendingMachine.inventoryRefill('Milk', 3)).toEqual('error')
         })
         it('Should return the object of the item that has been refilled', () => {
             expect(vendingMachine.inventoryRefill('Galaxy', 10)).toEqual(
-                {'count': 10, 'name': 'Galaxy'
+                {'count': 15,
+                'name': 'Galaxy'
             })
         })
         it('Should throw an error if the item is not a string', () => {
-            expect(vendingMachine.inventoryRefill(10, 3)).toEqual('Not valid!')
+            expect(vendingMachine.inventoryRefill(10, 3)).toEqual('error')
         })
         it('Should throw an error if amount is not an integer', () => {
-            expect(vendingMachine.inventoryRefill('Galaxy', 'two')).toEqual(
-                'Please insert appropiate numbers to indicate amount'
-            )
+            expect(vendingMachine.inventoryRefill('Galaxy', 'two')).toEqual('error')
+        })
+    })
+    // resupply
+    describe('When change is resupplied to the machine', () => {
+        it('Should return an updated count of the coins available', () => {
+          expect(vendingMachine.changeResupply('Nickel', 10)).toEqual({
+            name: 'Nickel',
+            count: 20
+          })
+        })
+        it('Should throw an error if the coin is not found', () => {
+          expect(vendingMachine.changeResupply('Lira', 5)).toEqual('error')
+        })
+        it('Should throw an error if coin is not a string', () => {
+          expect(vendingMachine.changeResupply(10, 10)).toEqual('error')
+        })
+        it('should throw an error if amount is not an integer', () => {
+            expect(vendingMachine.changeResupply('Nickle', 'ten')).toEqual('error')
         })
     })
 })
